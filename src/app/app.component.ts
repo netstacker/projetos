f957cbad9a9d8c8054ec9d6554828cd4bd4a0471
netstacker <netstacker@gmail.com>
2020-07-11 21:19:01 -0300

    2 files modified, 1 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component, VERSION } from '@angular/core';
+import { OfertaService } from './oferta.service';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
+
 export class AppComponent  {
+
   name = 'Angular ' + VERSION.major;
+  constructor(private ofertaservice:OfertaService){
+
+  }
+  ngOnInit(){
+    console.log(this.ofertaservice.getOfertas());
+  }
 }
---
+++ src/app/oferta.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class OfertaService {
+
+  constructor() { }
+
+
+public getOfertas():Array<string>{
+  var Ofertas = ['asas','asas'];
+  return Ofertas
+}
+
+}
